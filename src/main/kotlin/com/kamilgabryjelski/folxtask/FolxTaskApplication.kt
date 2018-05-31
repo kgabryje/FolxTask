fb3ceb65aa75c2a23581a8a9a52ae9663cbf30d7
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2018-05-31 19:30:46 +0200

    SpringBootAplication for server done

---
+++ src/main/kotlin/com/kamilgabryjelski/folxtask/FolxTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.kamilgabryjelski.folxtask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FolxTaskApplication
+
+fun main(args: Array<String>) {
+    runApplication<FolxTaskApplication>(*args)
+}
