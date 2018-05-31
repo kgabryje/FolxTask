e4b660b536935726626a176e68bb3d48e115f917
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2018-05-31 19:27:30 +0200

    Some custom exceptions for api endpoints

--- src/main/kotlin/com/kamilgabryjelski/folxtask/exceptions/NameAlreadyExists.kt
+++ src/main/kotlin/com/kamilgabryjelski/folxtask/exceptions/NameAlreadyExists.kt
@@ -3,5 +3,5 @@ package com.kamilgabryjelski.folxtask.exceptions
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(HttpStatus.BAD_REQUEST, reason = "ID must be specified")
-class IDNotSpecified: RuntimeException()
+@ResponseStatus(HttpStatus.BAD_REQUEST, reason = "Product name must be unique")
+class NameAlreadyExists: RuntimeException()
