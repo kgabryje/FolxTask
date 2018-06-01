31b706545b8aed2a5c408b0c96eec1490f9eb010
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2018-06-01 18:31:32 +0200

    Changed http status response of IDNotFound exception

--- src/main/kotlin/com/kamilgabryjelski/folxtask/exceptions/IDNotFound.kt
+++ src/main/kotlin/com/kamilgabryjelski/folxtask/exceptions/IDNotFound.kt
@@ -3,5 +3,5 @@ package com.kamilgabryjelski.folxtask.exceptions
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(HttpStatus.BAD_REQUEST, reason = "ID must be specified")
+@ResponseStatus(HttpStatus.NOT_FOUND, reason = "ID not found")
 class IDNotFound(message: String = ""): Exception(message)
