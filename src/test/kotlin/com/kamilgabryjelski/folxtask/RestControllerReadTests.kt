79cee1cc34ee51228259c1d900e2a8a8cc3c1bfe
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2018-06-01 18:30:48 +0200

    Controller read tests

---
+++ src/test/kotlin/com/kamilgabryjelski/folxtask/RestControllerReadTests.kt
@@ -0,0 +1,109 @@
+package com.kamilgabryjelski.folxtask
+
+import com.kamilgabryjelski.folxtask.constants.UriConstants
+import com.kamilgabryjelski.folxtask.model.Product
+import com.kamilgabryjelski.folxtask.model.ProductStatus
+import com.kamilgabryjelski.folxtask.server.ProductController
+import com.kamilgabryjelski.folxtask.server.ProductRepository
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.skyscreamer.jsonassert.JSONAssert
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MvcResult
+import org.springframework.test.web.servlet.RequestBuilder
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.util.*
+
+@RunWith(SpringRunner::class)
+@WebMvcTest(value = [(ProductController::class)], secure = false)
+class RestControllerReadTests {
+
+    @MockBean
+    @Autowired
+    lateinit var productRepository: ProductRepository
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+
+    @Test
+    fun testReadAllProducts() {
+        val mockProductList: List<Product> = listOf(
+                Product(1, "prod1", 123F, ProductStatus.INSTOCK),
+                Product(2, "prod2", 456F),
+                Product(3, status = ProductStatus.OUTOFSTOCK)
+                )
+        Mockito.`when`(productRepository.findAll()).thenReturn(mockProductList)
+
+        val requestBuilder: RequestBuilder = MockMvcRequestBuilders.get(UriConstants.READALL)
+        val result: MvcResult = mockMvc.perform(requestBuilder).andExpect(status().isOk).andReturn()
+        val expected = """
+            [{id: 1, name: prod1, price: 123, status: INSTOCK},
+            {id: 2, name: prod2, price: 456, status: WITHDRAWN},
+            {id: 3, name: "", price: 0, status: OUTOFSTOCK}]
+            """
+                .trimIndent()
+
+        JSONAssert.assertEquals(expected, result.response.contentAsString, false)
+    }
+
+    @Test
+    fun testReadAllProducts_Empty() {
+        val mockProductList: List<Product> = emptyList()
+        Mockito.`when`(productRepository.findAll()).thenReturn(mockProductList)
+
+        val requestBuilder: RequestBuilder = MockMvcRequestBuilders.get(UriConstants.READALL)
+        val result: MvcResult = mockMvc.perform(requestBuilder).andExpect(status().isOk).andReturn()
+        val expected = "[]"
+
+        JSONAssert.assertEquals(expected, result.response.contentAsString, false)
+    }
+
+    @Test
+    fun testReadByID() {
+        val id: Long = 1
+        val mockProduct: Optional<Product> = Optional.of(Product(1, "prod1", 123F, ProductStatus.INSTOCK))
+        Mockito.`when`(productRepository.findById(id)).thenReturn(mockProduct)
+
+        val requestBuilder: RequestBuilder = MockMvcRequestBuilders.get("${UriConstants.READBYID}?id=$id")
+        val result: MvcResult = mockMvc.perform(requestBuilder).andExpect(status().isOk).andReturn()
+        val expected = "{id: 1, name: prod1, price: 123, status: INSTOCK}"
+        JSONAssert.assertEquals(expected, result.response.contentAsString, false)
+    }
+
+    @Test
+    fun testReadByID_IDNotFound() {
+        val id: Long = 4
+        Mockito.`when`(productRepository.findById(id)).thenReturn(Optional.empty())
+
+        val requestBuilder: RequestBuilder = MockMvcRequestBuilders.get("${UriConstants.READBYID}?id=$id")
+        mockMvc.perform(requestBuilder).andExpect(status().isNotFound)
+    }
+
+    @Test
+    fun testReadByName() {
+        val name = "prod1"
+        val mockProduct: Optional<Product> = Optional.of(Product(1, "prod1", 123F, ProductStatus.INSTOCK))
+        Mockito.`when`(productRepository.findProductByName(name)).thenReturn(mockProduct)
+
+        val requestBuilder: RequestBuilder = MockMvcRequestBuilders.get("${UriConstants.READBYNAME}?name=$name")
+        val result: MvcResult = mockMvc.perform(requestBuilder).andExpect(status().isOk).andReturn()
+        val expected = "{id: 1, name: prod1, price: 123, status: INSTOCK}"
+        JSONAssert.assertEquals(expected, result.response.contentAsString, false)
+    }
+
+    @Test
+    fun testReadByName_NoSuchProduct() {
+        val name = "prod4"
+        Mockito.`when`(productRepository.findProductByName(name)).thenReturn(Optional.empty())
+
+        val requestBuilder: RequestBuilder = MockMvcRequestBuilders.get("${UriConstants.READBYNAME}?name=$name")
+        mockMvc.perform(requestBuilder).andExpect(status().isNotFound)
+    }
+}
