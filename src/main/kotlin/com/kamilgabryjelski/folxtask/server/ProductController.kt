a62b617d60084194170b603e11d0230bc43c9384
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2018-06-01 16:20:45 +0200

    refactoring of CRUD methods, throwing proper exceptions

--- src/main/kotlin/com/kamilgabryjelski/folxtask/server/ProductController.kt
+++ src/main/kotlin/com/kamilgabryjelski/folxtask/server/ProductController.kt
@@ -1,11 +1,15 @@
 package com.kamilgabryjelski.folxtask.server
 
-import com.kamilgabryjelski.folxtask.exceptions.IDNotSpecified
+import com.kamilgabryjelski.folxtask.exceptions.IDNotFound
 import com.kamilgabryjelski.folxtask.exceptions.NameAlreadyExists
+import com.kamilgabryjelski.folxtask.exceptions.NoSuchProduct
 import com.kamilgabryjelski.folxtask.exceptions.WithdrawnProductNotAllowed
 import com.kamilgabryjelski.folxtask.model.Product
 import com.kamilgabryjelski.folxtask.model.ProductStatus
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -15,49 +19,56 @@ class ProductController {
 
     @Transactional
     @RequestMapping("/createProduct", method = [RequestMethod.PUT])
-    fun createProduct(@RequestBody product: Product) {
-        if (product.status == ProductStatus.WITHDRAWN)
-            throw WithdrawnProductNotAllowed()
-        if (productRepository.findProductByName(product.name).isPresent)
-            throw NameAlreadyExists()
-        productRepository.save(product)
-    }
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createProduct(@RequestBody product: Product) =
+        when {
+            product.status == ProductStatus.WITHDRAWN -> throw WithdrawnProductNotAllowed()
+            productRepository.findProductByName(product.name).isPresent -> throw NameAlreadyExists()
+            else -> productRepository.save(product)
+        }
 
     @Transactional
     @RequestMapping("/readAll", method = [RequestMethod.GET])
-    fun readAllProducts(): List<Product> {
-        return productRepository.findAll() as List<Product>
-    }
+    fun readAllProducts(): List<Product> = productRepository.findAll() as List<Product>
+
     @Transactional
     @RequestMapping("/readByID", method = [RequestMethod.GET])
-    fun readProductByID(@RequestParam(value = "id") id: Long): Product {
-        return productRepository.findById(id).get()
-    }
+    fun readProductByID(@RequestParam(value = "id") id: Long): Product =
+            productRepository.findById(id).takeIf { it.isPresent }?.get() ?: throw NoSuchProduct()
 
     @Transactional
     @RequestMapping("/readByName", method = [RequestMethod.GET])
-    fun readProductByName(@RequestParam(value = "name") name: String): Product {
-        return productRepository.findProductByName(name).get()
-    }
+    fun readProductByName(@RequestParam(value = "name") name: String): Product =
+            productRepository.findProductByName(name).takeIf { it.isPresent }?.get() ?: throw NoSuchProduct()
 
     @Transactional
     @RequestMapping("/updateProduct", method = [RequestMethod.POST])
-    fun updateProduct(@RequestBody product: Product) {
-         if (productRepository.findById(product.id).isPresent)
-             productRepository.save(product)
-        else
-             throw IDNotSpecified()
+    fun updateProduct(@RequestBody product: Product): ResponseEntity<Unit> {
+        if (!productRepository.findById(product.id).isPresent)
+            throw IDNotFound()
+
+        productRepository.findProductByName(product.name).takeUnless {
+            it.isPresent && it.get().id != product.id
+        } ?: throw NameAlreadyExists()
+
+        productRepository.save(product)
+        return ResponseEntity.ok().build()
     }
 
     @Transactional
     @RequestMapping("/deleteByID", method = [RequestMethod.DELETE])
-    fun deleteProductByID(@RequestParam(value = "id") id: Long) {
-        productRepository.deleteById(id)
-    }
+    fun deleteProductByID(@RequestParam(value = "id") id: Long) =
+            when {
+                !productRepository.findById(id).isPresent -> throw NoSuchProduct()
+                else -> productRepository.deleteById(id)
+            }
+
 
     @Transactional
     @RequestMapping("/deleteByName", method = [RequestMethod.DELETE])
-    fun deleteProductByName(@RequestParam(value = "name") name: String) {
-        productRepository.deleteProductByName(name)
-    }
+    fun deleteProductByName(@RequestParam(value = "name") name: String) =
+            when {
+                !productRepository.findProductByName(name).isPresent -> throw NoSuchProduct()
+                else -> productRepository.deleteProductByName(name)
+            }
 }
