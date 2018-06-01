9993fee0fa3f9d6057a50e414939347f733ff342
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2018-06-01 16:19:44 +0200

    Added @Transactional annotations in rest controller

--- src/main/kotlin/com/kamilgabryjelski/folxtask/server/ProductController.kt
+++ src/main/kotlin/com/kamilgabryjelski/folxtask/server/ProductController.kt
@@ -13,6 +13,7 @@ class ProductController {
     @Autowired
     lateinit var productRepository: ProductRepository
 
+    @Transactional
     @RequestMapping("/createProduct", method = [RequestMethod.PUT])
     fun createProduct(@RequestBody product: Product) {
         if (product.status == ProductStatus.WITHDRAWN)
@@ -22,20 +23,24 @@ class ProductController {
         productRepository.save(product)
     }
 
+    @Transactional
     @RequestMapping("/readAll", method = [RequestMethod.GET])
     fun readAllProducts(): List<Product> {
         return productRepository.findAll() as List<Product>
     }
+    @Transactional
     @RequestMapping("/readByID", method = [RequestMethod.GET])
     fun readProductByID(@RequestParam(value = "id") id: Long): Product {
         return productRepository.findById(id).get()
     }
 
+    @Transactional
     @RequestMapping("/readByName", method = [RequestMethod.GET])
     fun readProductByName(@RequestParam(value = "name") name: String): Product {
         return productRepository.findProductByName(name).get()
     }
 
+    @Transactional
     @RequestMapping("/updateProduct", method = [RequestMethod.POST])
     fun updateProduct(@RequestBody product: Product) {
          if (productRepository.findById(product.id).isPresent)
@@ -44,11 +49,13 @@ class ProductController {
              throw IDNotSpecified()
     }
 
+    @Transactional
     @RequestMapping("/deleteByID", method = [RequestMethod.DELETE])
     fun deleteProductByID(@RequestParam(value = "id") id: Long) {
         productRepository.deleteById(id)
     }
 
+    @Transactional
     @RequestMapping("/deleteByName", method = [RequestMethod.DELETE])
     fun deleteProductByName(@RequestParam(value = "name") name: String) {
         productRepository.deleteProductByName(name)
