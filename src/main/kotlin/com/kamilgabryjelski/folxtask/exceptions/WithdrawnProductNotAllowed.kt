37e8f3417ca648f6c35883973603947602e3cbe1
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2018-06-01 16:18:50 +0200

    WithdrawnProductNotAllowed - default message,  inherits from Exception

--- src/main/kotlin/com/kamilgabryjelski/folxtask/exceptions/WithdrawnProductNotAllowed.kt
+++ src/main/kotlin/com/kamilgabryjelski/folxtask/exceptions/WithdrawnProductNotAllowed.kt
@@ -4,4 +4,4 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(HttpStatus.BAD_REQUEST, reason = "Can't create a product with status: Withdrawn")
-class WithdrawnProductNotAllowed: RuntimeException()
+class WithdrawnProductNotAllowed(message: String = ""): Exception(message)
