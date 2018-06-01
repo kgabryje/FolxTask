b9987ece62040dfdeb9d0530e1ae18c31134a50a
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2018-06-01 16:18:17 +0200

    ID not found exception

--- src/main/kotlin/com/kamilgabryjelski/folxtask/exceptions/IDNotFound.kt
+++ src/main/kotlin/com/kamilgabryjelski/folxtask/exceptions/IDNotFound.kt
@@ -4,4 +4,4 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(HttpStatus.BAD_REQUEST, reason = "ID must be specified")
-class IDNotSpecified: RuntimeException()
+class IDNotFound(message: String = ""): Exception(message)
