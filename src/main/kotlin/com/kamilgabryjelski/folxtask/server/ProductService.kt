c5fd0581236b9dc26adf8fd9270781d8d8241878
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2018-06-02 09:23:44 +0200

    implemented ProductService

---
+++ src/main/kotlin/com/kamilgabryjelski/folxtask/server/ProductService.kt
@@ -0,0 +1,20 @@
+package com.kamilgabryjelski.folxtask.server
+
+import com.kamilgabryjelski.folxtask.model.Product
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class ProductService {
+    @Autowired
+    lateinit var productRepository: ProductRepository
+
+    fun createOrUpdate(product: Product) = productRepository.save(product)
+
+    fun findAll() = productRepository.findAll()
+    fun findByID(id: Long) = productRepository.findById(id)
+    fun findByName(name: String) = productRepository.findProductByName(name)
+
+    fun deleteByID(id: Long) = productRepository.deleteById(id)
+    fun deleteByName(name: String) = productRepository.deleteProductByName(name)
+}
